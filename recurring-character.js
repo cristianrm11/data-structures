813dfbb4c6d2e4aaf1285a27bb4304aed3d1d505
Christian R <cristianrm11@gmail.com>
2021-06-05 13:35:35 -0600

    solve the recurring character exercise with hash table

--- recurring-character.js
+++ recurring-character.js
@@ -10,5 +10,22 @@ function firstRecurringCharacter(input) {
 }
 
 const firstCharater = firstRecurringCharacter([1, 2, 3, 4, 5, 9, 5]);
+console.log('using for... this is low performance sln', firstCharater);
 
-console.log(firstCharater);
+/// now solve the previous exercise with hash tables to increment performance
+
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+}
+
+console.log(
+  'using hash firstRecurringCharacter',
+  firstRecurringCharacter2(['a', 'b', 'u', 'y', 'e', 'z', 'x', 'y'])
+);
