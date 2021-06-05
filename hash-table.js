0e971d9cb73d569d5bb8bb3506f36e1e3824bf0f
Christian R <cristianrm11@gmail.com>
2021-06-05 13:00:17 -0600

    implement get and set

--- hash-table.js
+++ hash-table.js
@@ -2,7 +2,7 @@ class HashTable {
   constructor(size) {
     this.data = new Array(size);
   }
-
+  // underscode to make it private... just for understanding
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
@@ -11,5 +11,35 @@ class HashTable {
     return hash;
   }
 
-  // implemente get and set
+  // implements get and set
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+  }
 }
+
+const myHastTable = new HashTable(50);
+myHastTable.set('grape', 1000);
+myHastTable.set('apple', 2000);
+myHastTable.set('orange', 3000);
+myHastTable.set('oranges', 3000);
+
+console.log(myHastTable.get('orange'));
+
+console.log(JSON.stringify(myHastTable));
